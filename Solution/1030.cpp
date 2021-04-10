f839deb32c39e08f6e27987073feb388eb690eb5
SecretHamster04 <fastmu2004@gmail.com>
2021-04-10 21:29:18 +0900

    2021-04-10

---
+++ Solution/1030.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main ()
+{
+    long long int lli;
+    scanf("%lld", &lli);
+    printf("%lld", lli);
+}
---
+++ Solution/1072.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+  
+int main()
+{    
+    int cnt;
+    int n;
+      
+    scanf("%d", &cnt);
+      
+    again:
+    if(cnt-- != 0)
+    {
+        scanf("%d", &n);
+        printf("%d\n", n);
+        goto again;
+    }
+      
+    return 0;
+}
+
+
+
